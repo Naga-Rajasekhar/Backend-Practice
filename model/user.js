f257c6295758202eccbaa58804e4d7cb6c52ed9d
Naga-Rajasekhar <rajasekhar9860@gmail.com>
2023-01-15 22:28:32 +0530

    prepared model schema for user

---
+++ model/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    default: null,
+    min: 3,
+    max: 20,
+  },
+  lastname: {
+    type: String,
+    default: null,
+    min: 1,
+    max: 20,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  token: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
